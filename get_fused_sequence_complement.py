5a2d9ceb757d61ed59a8f25b785fa82d5d488c75
Reilly <reilly.ford@gmail.com>
2021-03-06 00:17:21 -0500

    finished (unless gradescope has a problem with it lol)

--- get_fused_sequence_complement.py
+++ get_fused_sequence_complement.py
@@ -1,5 +1,56 @@
 def get_fused_sequence_complement(sequence_a, sequence_b):
-    pass
+    """
+    This function receives two sequences of DNA and combines them, iterating over
+    the two sequences and alternating between characters from each one. If one
+    sequence is longer than the other, it will only add characters from both until
+    the shorter sequence runs out of characters, at which point it will simply add
+    the characters from the longer string onto the end. After it has completed
+    this, it passes the combo string to get_complement, which returns a complement
+    DNA string that this function then returns to its reference in main().
+
+    :param sequence_a: the first of the two sequences passed
+    :param sequence_b: the second of the two sequences passed
+    :return: the complement of the combo of the two strings received
+    """
+    combo_sequence = ""
+    if len(sequence_a) >= len(sequence_b):
+        longer_sequence = sequence_a
+        shorter_sequence = sequence_b
+    else:
+        longer_sequence = sequence_b
+        shorter_sequence = sequence_a
+    for x in range(len(longer_sequence)):
+        if longer_sequence[x] == "A" or longer_sequence[x] == "T" or longer_sequence[x] == "C" or longer_sequence[x] == "G":
+            combo_sequence = combo_sequence + longer_sequence[x]
+        if len(shorter_sequence) > x:
+            if shorter_sequence[x] in ("A", "T", "C", "G"):
+                combo_sequence = combo_sequence + shorter_sequence[x]
+    comp_sequence = get_complement(combo_sequence)
+    return comp_sequence
+
+
+def get_complement(combo_sequence):
+    """
+    This function receives a combined DNA strand from get_fused_sequence_complement
+    and iterates over the string, constructing a new string where each character
+    is the pairing nucleotide to the original character.
+
+    :param combo_sequence: the already-combined sequence of the two initial
+    sequences
+    :return: the complement sequence to the one passed in
+    """
+    comp_sequence = ""
+    for x in range(len(combo_sequence)):
+        char = combo_sequence[x]
+        if char == "A":
+            comp_sequence += "T"
+        elif char == "C":
+            comp_sequence += "G"
+        elif char == "T":
+            comp_sequence += "A"
+        elif char == "G":
+            comp_sequence += "C"
+    return comp_sequence
 
 def main():
     """
--- password_checker.py
+++ password_checker.py
@@ -1,13 +1,40 @@
 def is_password_valid(test_password):
-    pass
+    """
+    Accepts a password and tests to make sure that it meets all the
+    qualifications to be a valid password.
 
+    :param test_password: the password to be tested for its validity
+    :return: boolean for whether or not the password is valid
+    """
+    length = len(test_password)
+    if length >= 8:
+        num_lower = 0
+        num_upper = 0
+        num_numbers = 0
+        num_chars = 0
+        for x in range(length):
+            if test_password[x].islower():
+                num_lower += 1
+            elif test_password[x].isupper():
+                num_upper += 1
+            elif test_password[x].isnumeric():
+                num_numbers += 1
+            elif test_password[x] == "!" or test_password[x] == "@" or test_password[x] == "!" or test_password[x] == "#" or test_password[x] == "$":
+                num_chars += 1
+        if num_lower >= 2 and num_upper >= 2 and num_numbers >= 1 and num_chars >= 1:
+            return True
+    return False
 
 
 def main():
     """
-    Just some sample behavior. Feel free to try your own!
+    Passes the password to be tested into is_password_valid,
+    receives a boolean value back, and prints the appropriate
+    message to inform the user of their password's validity.
+
+    :return: none
     """
-    test_password = "NAit1!"
+    test_password = "NAit1ppp!"
 
     if is_password_valid(test_password):
         print("This is a valid password!")
